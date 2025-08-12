df611134530161d8ecbf8a590bd05e404faa364f
Manish.Kumar10 <sangwanmanish2003@gmail.com>
2025-08-12 21:04:00 +0530

    added a fuck all commer

--- Ecommerce-Backend/src/main/java/app/ecom/EcommerceApplication.java
+++ Ecommerce-Backend/src/main/java/app/ecom/EcommerceApplication.java
@@ -2,7 +2,7 @@ package app.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//manish chutiya hai
 @SpringBootApplication
 public class EcommerceApplication {
 
